976ddf7b78edea5f197d591999d0f9027286e8d7
Edgars Sidorovs <126489847+Edgarsid@users.noreply.github.com>
2023-02-27 17:46:17 +0200

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,15 @@
+void Pievienot_ierakstu ();
+
+void Dzest_ierakstu ();
+
+void Izvadit_visus_ierakstus ();
+
+void Atrast_ierakstu ();
+
+void Izmainit_konta_balansu ();
+
+void Izvadit_paradniekus ();
+
+void Saskaitit_ierakstus ();
+
+void Restartet_datu_bazi ();
